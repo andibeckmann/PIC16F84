7cc88a3a8177601040f1efc86afe4c398c9f354a
Andreas Beckmann <beckmann.herxheim@web.de>
2014-03-31 12:15:55 +0200

    NOP NOP NOP

---
+++ Simulator PIC16F84/RegisterFileMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator_PIC16F84
+{
+    class RegisterFileMap
+    {
+        private byte[] register = new byte[24];
+    }
+}
