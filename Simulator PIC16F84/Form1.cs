9d6a98b99befad7a32aab45f7ce408229ff7c0e9
Olli1511 <oliver.hamboerger@gmail.com>
2014-03-31 10:23:03 +0200

    Added Button

--- Simulator PIC16F84/Form1.cs
+++ Simulator PIC16F84/Form1.cs
@@ -21,5 +21,10 @@ namespace Simulator_PIC16F84
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("BLALFSDGSRGRDSH");
+        }
     }
 }
