d862b95d35421b4ac4bf7a60edcb76cef118494e
Andreas Beckmann <beckmann.herxheim@web.de>
2014-04-07 09:34:24 +0200

    Erste Arbeit am Register

--- Simulator PIC16F84/Form1.Designer.cs
+++ Simulator PIC16F84/Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 3, 0, 3);
-            this.menuStrip1.Size = new System.Drawing.Size(621, 25);
+            this.menuStrip1.Size = new System.Drawing.Size(1056, 25);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -99,7 +99,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(621, 230);
+            this.ClientSize = new System.Drawing.Size(1056, 392);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MainMenuStrip = this.menuStrip1;
