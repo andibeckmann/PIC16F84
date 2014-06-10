4d586f1953f6bbae1e0a99f08d5793e91f68a7d7
Andreas Beckmann <beckmann.herxheim@web.de>
2014-06-10 17:20:01 +0200

    TODO-Liste und Tabelle auf neuesten Stand

--- Simulator PIC16F84/ProgramMemoryView.Designer.cs
+++ Simulator PIC16F84/ProgramMemoryView.Designer.cs
@@ -55,7 +55,7 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(784, 462);
+            this.dataGridView1.Size = new System.Drawing.Size(804, 462);
             this.dataGridView1.TabIndex = 0;
             // 
             // BreakPoints
@@ -111,7 +111,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 462);
+            this.ClientSize = new System.Drawing.Size(804, 462);
             this.Controls.Add(this.dataGridView1);
             this.Location = new System.Drawing.Point(265, 0);
             this.Name = "ProgramMemoryView";
